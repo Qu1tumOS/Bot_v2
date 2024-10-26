e9890f349a627324311f7f2d41884d8d9ed74ff4
Qu1tum <vladimirzagotovkin@yandex.ru>
2024-10-26 20:53:08 +0300

    fix error

--- Handlers/lessons/today_more_info.py
+++ Handlers/lessons/today_more_info.py
@@ -71,9 +71,9 @@ async def log(callback: CallbackQuery):
     redis_connect = redis.Redis(host='localhost')
     
     page = redis_connect.get(name=id)
-    page_day_number = (date.today() + timedelta(page)).weekday()
     if not page:
         redis_connect.set(name=id, value=0)
+        page_day_number = (date.today() + timedelta(page)).weekday()
     elif page_day_number == 6:
         page += 1
     else:
